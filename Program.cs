7ec65b6fafa39eeaf20b574d6a07a6853fe053d0
_ttd <{{razyagamer@gmail.com}}>
2023-09-08 13:51:14 +0300

    second commit

--- Program.cs
+++ Program.cs
@@ -7,7 +7,7 @@ namespace HelloWorld
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello Zazymia! Happy 2007!");
+            Console.WriteLine("Hello Zazymia! Happy 2007!!!");
         }
     }
 }
